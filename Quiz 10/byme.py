9ae51ccc5062c01b41fc6e526880fb47b9efd742
kangsowon <wishkang21@gmail.com>
2024-12-12 16:58:50 +0900

    Quiz 10 풀이(DVT-119)

---
+++ Quiz 10/byme.py
@@ -0,0 +1,11 @@
+# 내 답안
+def sign():
+    print("이 프로그램은 나도코딩에 의해 만들어졌습니다.")
+    print("유튜브 : http://youtube.com")
+    print("이메일 : nadocoding@gmail.com")
+
+# 나도코딩 답안
+# def sign():
+#     print("이 프로그램은 나도코딩에 의해 만들어졌습니다.")
+#     print("유튜브 : http://youtube.com")
+#     print("이메일 : nadocoding@gmail.com")
